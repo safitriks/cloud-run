96363d447f6bcb61113ded124c8d274a90f5b19b
safitriks <119040522+safitriks@users.noreply.github.com>
2024-05-10 22:50:06 +0700

    Minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + '/src/index.html');
+	res.sendFile(__dirname + '/src/form.html');
 });
 
 const PORT = process.env.PORT || 8080;
